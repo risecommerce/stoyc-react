d26dfe76e259176f51e65cf3fe8bd30d78b277de
risecommerce <risecommerce@gmail.com>
2024-02-28 12:41:14 +0530

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { Routes, Route } from "react-router-dom";
+import React from "react";
+import AnimatedCursor from "react-animated-cursor"
+
+
+// Layout
+import Layout from "./layout/Layout";
+
+// pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Prices from "./pages/Prices";
+import Services from "./pages/Service";
+import Projects from "./pages/Project";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <AnimatedCursor
+        innerSize={8}
+        outerSize={35}
+        innerScale={1}
+        outerScale={2}
+        outerAlpha={0}
+        hasBlendMode={true}
+        innerStyle={{
+          backgroundColor: 'var(--cursor-color)'
+        }}
+        outerStyle={{
+          border: '3px solid var(--cursor-color)'
+        }}
+      />
+      
+        <Routes>
+          <Route path="/" element={<Home />} exact />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/prices" element={<Prices />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+     
+    </Layout>
   );
-}
+};
 
 export default App;
